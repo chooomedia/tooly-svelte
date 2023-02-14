d5b72bb5cce6270e17e0cdc390076a2d354a6858
chooomedia <9967192+chooomedia@users.noreply.github.com>
2023-02-15 00:25:22 +0100

    added gsap for svg tweening animations

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,9 @@
 <script context="module">
-	import Chat from './Chat.svelte';
-	import CtaBox from './Ctabox.svelte';
-	import Box from './Box.svelte';
-	import Countdown from './Countdown.svelte';
-	import FixedMenu from './FixedMenu.svelte';
-	import Fooorms from './Fooorms.svelte';
-	import ImageBubbles from './ImageBubbles.svelte';
+	import Chat from './widgets/Chat.svelte';
+	import TrackingSettings from './widgets/TrackingSettings.svelte';
 	export const Widgets = {
 		"Chat": Chat,
-		"CtaBox": CtaBox,
-		"Box": Box,
-		"Countdown": Countdown,
-		"FixedMenu": FixedMenu,
-		"Fooorms": Fooorms,
-		"ImageBubbles": ImageBubbles
+		"TrackingSettings": TrackingSettings
 	}
 </script>
 
@@ -23,17 +13,21 @@
 	import Flexbox from './Flexbox.svelte';
 	import { content } from './content.js';
 	import { lazyLoad } from './lazyload.js';
+	import CtaBox from './Ctabox.svelte';
+	import Box from './Box.svelte';
+	import Countdown from './Countdown.svelte';
+	import FixedMenu from './FixedMenu.svelte';
+	import Fooorms from './Fooorms.svelte';
+	import ImageBubbles from './ImageBubbles.svelte';
 	import AnimatedBackground from './Animated-background.svelte';
 	import BackgroundMaks from './Background-maks.svelte';
-	import { close } from './shapes.js';
 	import StickyContent from './StickyContent.svelte';
 	import { elasticIn, elasticOut } from 'svelte/easing';
     import { fly, fade } from 'svelte/transition';
+  	import Ctabox from './Ctabox.svelte';
 
 	let stickToTop = true;
-	let selectedLink = undefined;
 	let currentSection = "Forms";
-	let showMenu = true;
 
 	/* This function reacts on every change of the block change "$darkmodestore" */
 	$:{
@@ -49,22 +43,10 @@
 	}
 	function onInvisible(e){
 	}
-
-	let menuKeyboard = false;
-
-	function keyEventListener(e) {
-		content.de.links.forEach((link) => {
-			if (e.key === link.id) {
-				showMenu = !showMenu;
-				menuKeyboard = true;
-			}
-		});
-	};
-
 </script>
 
 <Header brandName={'Tooly'}></Header>
-<main>
+<main class="col-xs-12 col-lg-12">
 	<!--et_pb_section et_pb_section_0 et-light-mode-capable et_pb_section_parallax et_pb_with_background et_section_regular-->
 	<section id="landingPage">
 		<Flexbox>
@@ -92,7 +74,7 @@
 				{/if}
 			</StickyContent>
 			<div slot="right">
-				<Box on:visible={onVisible} on:invisible={onInvisible} name={"Forms"} direction={'flex-column'}>
+				<Box on:visible={onVisible} on:invisible={onInvisible} name={"Forms"} direction={'flex-column'} height={100}>
 					<CtaBox headline={content.de.ctaBox.headline} subline={content.de.ctaBox.subline}></CtaBox>
 					<Countdown from="2023-02-07 08:00:00" dateFormat="YYYY-DD-MM H:m:s" zone="Europe/Berlin" let:remaining>
 						<div class="flex-box">
@@ -111,7 +93,7 @@
 					</Countdown>
 					<Fooorms></Fooorms>
 				</Box>
-				<Box on:visible={onVisible} on:invisible={onInvisible} name={"Steps"} direction={'flex-row'}>
+				<Box on:visible={onVisible} on:invisible={onInvisible} name={"Steps"} direction={'row-wraped'} height={100}>
 					{#each content.de.stepBox.steps as step, i }
 						{#if currentSection == "Steps"}
 							<div class="col-lg-3" name="step" in:fly="{{y: 300, delay: i * 700, duration: 700, elasticIn}}" out:fly="{{y: 300, delay: i * 700, duration: 700, elasticOut}}">
@@ -124,68 +106,39 @@
 						{/if}
 					{/each}
 				</Box>
-				<Box on:visible={onVisible} on:invisible={onInvisible} name={"Roadmap"} direction={'flex-row'}>
-					{#each content.de.roadMap.steps as roadMapStep, f }
-						{#if currentSection == "Roadmap"}
-							<div class="col-lg-3" name="step" in:fly="{{y: 300, delay: f * 700, duration: 700, elasticIn}}" out:fly="{{y: 300, delay: f * 700, duration: 700, elasticOut}}">
-							<div class="icon active">
-								<img use:lazyLoad={roadMapStep.iconSrc} src={roadMapStep.iconSrc} alt="Roadmap Icon" />
-							</div>
-							<h2>{roadMapStep.headline}</h2>
-							<p>{roadMapStep.text}</p>
-							</div>
-						{/if}
-					{/each}
-					<div slot="cta">
-						<a href="#logoSvg" type="button">Mitwirken</a>
-					</div>
-				</Box>
 			</div>
 		</Flexbox>
 	</section>
+	<section id="optional">
+		<CtaBox headline={content.de.roadMap.headline} subline={content.de.roadMap.subline} boxed button={'Jetzt mitmachen'}></CtaBox>
+		<Box on:visible={onVisible} on:invisible={onInvisible} name={"Roadmap"} direction={'row'} height={33}>
+			{#each content.de.roadMap.steps as roadMapStep, f }
+				{#if currentSection == "Roadmap"}
+					<div class="col-lg-3" name="step" in:fly="{{y: 300, delay: f * 700, duration: 700, elasticIn}}" out:fly="{{y: 300, delay: f * 700, duration: 700, elasticOut}}">
+					<div class="icon">
+						<img use:lazyLoad={roadMapStep.iconSrc} src={roadMapStep.iconSrc} alt="Roadmap Icon" />
+					</div>
+					<h2>{roadMapStep.headline}</h2>
+					<p>{roadMapStep.text}</p>
+					</div>
+				{/if}
+			{/each}
+		</Box>
+	</section>
 </main>
 
-<FixedMenu modalIsOpen={selectedLink} align={'bottom'}>
-<div slot="modal">
-	<header class="flex-box">
-		<img src="{selectedLink.imgSrc}" width="24px" height="24px" alt="icon chat" />
-			<h4>{selectedLink.title}</h4>
-		<button class="close-button" on:click={() => selectedLink = false}>
-			<svg id="icon-close" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="10px" height="10px" viewBox="0 0 121.31 122.876">
-				<path d="{close}" />
-			</svg>
-		</button>
-	</header>
-	<svelte:component this={Widgets[selectedLink.dialogContent]} />
-</div>
+<FixedMenu align={'bottom'}>
+	<div slot="modal" let:selectedLink={selectedLink}>
+		<svelte:component this={Widgets[selectedLink.dialogContent]}/>
+	</div>
 
-{#if showMenu}
-	{#each content.de.links as link, index}
-		<slot>
-			<a in:fly="{{y: -5, duration: 380, delay: 100, elasticIn}}"
-				href={link.href}
-				target={link.target}
-				class={link.class}
-				class:active={selectedLink == link}
-				rel={link.rel}
-				title={link.title}
-				on:click={() => {
-					if (selectedLink == link) {
-						selectedLink = undefined;
-					} else {
-						selectedLink = link;
-					}
-				}}
-				on:keydown={keyEventListener}>
-				<span class="index-label" aria-label="menu-index-{index + 1}" in:fade>
-					{index + 1}
-				</span>
-				<img src={link.imgSrc} alt={link.alt} />
-				{link.title}
-			</a>
-		</slot>
-	{/each}
-{/if}
+	<div class="fixed-menu-icon" slot="item" let:link={link} in:fly={{y: 57, duration: 300, delay: link?.id	 * 100}}>
+		<span class="index-label" aria-label="menu-index-{link?.id}">
+			{link?.id}
+		</span>
+		<img style="width: 28px; height: 28px;" src={link?.imgSrc} alt={link?.alt} />
+		{link?.title}
+	</div>
 </FixedMenu>
 
 <BackgroundMaks></BackgroundMaks>
@@ -193,22 +146,18 @@
 
 <style>
 .fixed-menu-icon {
-	width: 57.4px;
-	height: 52.4px;
-	display: flex;
-	line-height: 1;
-	border-radius: 15px;
-	transition: all 0.3s;
-	text-align: center;
-	font-size: 10px;
-	align-items: center;
-	flex-direction: column;
-	justify-content: space-evenly;
+    width: 57.4px;
+    height: 52.4px;
+    display: flex;
+    line-height: 1;
+    border-radius: 15px;
+    transition: all 0.3s;
+    text-align: center;
+    font-size: 10px;
+    align-items: center;
+    flex-direction: column;
+    justify-content: space-evenly;
 }
-.fixed-menu-icon > img {
-	width: 28px; height: 28px;
-}
-
 .index-label {
     position: absolute;
     top: 0;
@@ -221,4 +170,9 @@
     padding: 3px;
 }
 
+@media screen and (max-width:768px) {
+	.index-label {
+		display: none;
+	}
+}
 </style>
